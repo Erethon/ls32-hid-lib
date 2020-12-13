acb60dc573cee2b4f8fc3d403bc3e354df9676e0
Dionysis Grigoropoulos <dgrig@erethon.com>
2020-12-13 05:36:19 +0200

    lib: Split opening/sending messages to fuctions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-extern crate hidapi;
-
 mod charset;
+use hidapi::{HidApi, HidDevice, HidResult};
 use serde::Serialize;
 use std::convert::TryInto;
 
@@ -20,8 +19,15 @@ struct Header {
     boundary: [u8; 20],
 }
 
-pub fn send_msg(msg: &str, blink: bool, brightness: u8) {
-    let message: u8 = msg.len().try_into().unwrap();
+pub fn open_badge() -> HidResult<HidDevice> {
+    let hid = HidApi::new().unwrap();
+    Ok(hid
+        .open(VENDORID, PRODUCTID)
+        .expect("Failed to open device with VendorID and ProductID"))
+}
+
+pub fn send_msg(badge: HidDevice, msg: &str, blink: bool, brightness: u8) -> HidResult<usize> {
+    let msg_len: u8 = msg.len().try_into().expect("Message longer than then maximum length of 255 characters");
 
     let header = Header {
         preamble: [0x77, 0x61, 0x6e, 0x67, 0x00],
@@ -29,20 +35,17 @@ pub fn send_msg(msg: &str, blink: bool, brightness: u8) {
         blink: if blink { 0xff } else { 0x00 },
         mode: [0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
         length: [
-            0x00, message, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, msg_len, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00,
         ],
         ..Default::default()
     };
-    let hid = hidapi::HidApi::new().unwrap();
-    let badge = hid
-                .open(VENDORID, PRODUCTID)
-                .expect("Failed to open device with VendorID and ProductID");
-    let mut data = bincode::serialize(&header).unwrap();
+
+    let mut data = bincode::serialize(&header).expect("Failed to serialize header");
     for character in msg.chars() {
         for byte in charset::CHARSET.get::<str>(&character.to_string()).unwrap() {
             data.push(*byte);
         }
     }
-    let _led = badge.write(&data);
+    badge.write(&data)
 }
